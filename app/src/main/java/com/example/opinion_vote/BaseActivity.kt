dac428ee1468f4c068701daacc5bce4951a7fd1d
ichbong94 <ichbong94@gmail.com>
2022-06-06 12:36:03 +0900

    test

---
+++ app/src/main/java/com/example/opinion_vote/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.opinion_vote
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
