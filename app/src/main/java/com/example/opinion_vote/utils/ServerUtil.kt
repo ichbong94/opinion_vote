53ce25f8c705bc40268f0429a8088be797abbe42
ichbong94 <ichbong94@gmail.com>
2022-06-06 13:57:04 +0900

    1

---
+++ app/src/main/java/com/example/opinion_vote/utils/ServerUtil.kt
@@ -0,0 +1,14 @@
+package com.example.opinion_vote.utils
+
+class ServerUtil {
+
+    companion object{
+        val BASE_URL ="http://54.180.52.26"
+
+        fun postRequestLogin(){
+
+        }
+
+
+    }
+}
