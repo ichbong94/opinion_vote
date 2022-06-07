73c3d9f6c23e9cb0cd2be972d3f666e69b847548
ichbong94 <ichbong94@gmail.com>
2022-06-07 22:30:06 +0900

    자동 로그인 구현

--- app/src/main/java/com/example/opinion_vote/SplashActivity.kt
+++ app/src/main/java/com/example/opinion_vote/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.example.opinion_vote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.opinion_vote.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,25 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        3초후 검사 저장된 토큰이 있는 지
+
+        val myHadnler = Handler(Looper.getMainLooper())
+
+        myHadnler.postDelayed({
+
+            if (ContextUtil.getToken(mContext) == "") {
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+        }
+            else {
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)}
+             finish()
+
+
+
+        }, 3000)
 
     }
 
