2be9473943d71a48fb2a9b33be8358ef846ae33e
ichbong94 <ichbong94@gmail.com>
2022-06-07 22:17:49 +0900

    스를래시 화면 추가

--- app/src/main/java/com/example/opinion_vote/LoginActivity.kt
+++ app/src/main/java/com/example/opinion_vote/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.opinion_vote
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import com.example.opinion_vote.utils.ContextUtil
 import com.example.opinion_vote.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
@@ -41,6 +42,10 @@ class LoginActivity : BaseActivity() {
 
                     val code =jsonObj.getInt("code")
                     if(code ==200){
+
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val token = dataObj.getString("token")
+                        ContextUtil.setToken(mContext, token)
 
                         val myIntent = Intent(mContext, MainActivity::class.java)
                         startActivity(myIntent)
---
+++ app/src/main/java/com/example/opinion_vote/utils/ContextUtil.kt
@@ -0,0 +1,25 @@
+package com.example.opinion_vote.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object{
+        private val prefName = "ColosseumPref"
+        private val TOKEN ="TOKEn"
+
+        fun setToken(context: Context, token: String){
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(TOKEN, token).apply()
+        }
+
+        fun getToken(context: Context) : String{
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getString(TOKEN, "")!!
+
+
+
+        }
+
+    }
+}
